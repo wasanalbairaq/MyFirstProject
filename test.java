d6854e01d1d1827519b4d02de98655b39622c936
wasanalbairaq <wasanibrahim93@gmail.com>
2025-12-30 13:47:52 +0400

    Update test.java

--- test.java
+++ test.java
@@ -1,6 +1,6 @@
 public class Test {
   public static void main (String [] args) {
     System.out.println("Hello World!");
-    System.out.println("Engineer");
+    System.out.println("Engineer"); //i have just added that to see the change
   }
 }
